9c97202ab19246be3eddcf3f914f293ff1f6d8a9
dannydelott <dannydelott@gmail.com>
2015-05-16 12:38:18 -0700

    Add content div to AppView

--- views/AppView.js
+++ views/AppView.js
@@ -2,10 +2,12 @@ var AppView = Backbone.View.extend({
 
   id: 'container',
 
-  initialize: function(){
+  initialize: function() {
     this.render();
   },
-  render: function(){
+
+  render: function() {
+    this.$el.html('<div id="content"></div>');
     $('body').append(this.el);
     return this;
   }
