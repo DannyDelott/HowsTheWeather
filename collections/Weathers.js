c833de00ff1b1b82135d884cb02937d02232440b
dannydelott <dannydelott@gmail.com>
2015-05-16 02:00:23 -0700

    Move getData from model to collection

--- collections/Weathers.js
+++ collections/Weathers.js
@@ -3,9 +3,28 @@ var Weathers = Backbone.Collection.extend({
   model: Weather,
 
   addWeather: function(zipcode) {
-    this.add(new Weather({
-      zipcode: zipcode
-    }));
+
+    this.getData(
+      zipcode,
+      function(data) {
+        this.add(new Weather({
+          zipcode: zipcode,
+          city: data.name,
+          weather: data.main.temp + ' °F'
+        }));
+      }.bind(this)
+    );
+
+  },
+
+  getData: function(zipcode, callback) {
+
+    $.get(
+      'http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial',
+      function(data) {
+        callback(data);
+      }.bind(this)
+    );
   }
 
 });
--- models/Weather.js
+++ models/Weather.js
@@ -1,33 +1,11 @@
 var Weather = Backbone.Model.extend({
 
   defaults: {
-    zipcode: 'Enter your zip code',
+    zipcode: '',
     city: '',
     weather: ''
   },
 
-  initialize: function() {
+  initialize: function() {},
 
-    // set listeners for changes to the model data
-    this.on('change:zipcode', this.getData, this);
-  },
-
-  getData: function() {
-
-    // access the open weather api and update model properties
-    $.get(
-      'http://api.openweathermap.org/data/2.5/weather?zip=' + this.get('zipcode') + ',us&units=imperial',
-      function(data) {
-
-        // update model properties
-        this.set('city', data.name);
-        this.set('weather', data.main.temp + ' °F');
-
-        // trigger the changeTitle event on views who are listening for it.
-        this.trigger('changeTitle');
-
-      }.bind(this)
-    );
-
-  }
 });
