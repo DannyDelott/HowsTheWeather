1f795440c4ad882696de5ac05f63b918f6f55e71
dannydelott <dannydelott@gmail.com>
2015-05-23 00:35:15 -0700

    Update initialize property, update render property

--- js/views/ListView.js
+++ js/views/ListView.js
@@ -3,27 +3,27 @@ var ListView = Backbone.View.extend({
   id: 'list',
 
   initialize: function() {
-    this.render();
-    this.listenTo(this.collection, 'add', this.refreshList);
-    //this.collection.on('add', this.refreshList, this);
+    this.listenTo(this.collection, 'add', this.render);
+    //this.collection.on('add', this.render, this);
   },
 
   render: function() {
-    $('#content').append(this.el);
-  },
-
-  refreshList: function() {
 
     this.$el.empty();
 
-    var entries = this.collection.models.map(function(model) {
+    this.entries = this.collection.models.map(function(model) {
       return new EntryView({
         model: model
       });
     });
 
-    this.$el.append(entries);
+    var $els = this.entries.map(function(entry) {
+      return entry.$el;
+    });
 
+    this.$el.append($els);
+
+    return this;
   }
 
 });
