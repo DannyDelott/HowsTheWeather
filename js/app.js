8c2cf7ff3dafd6e2e4342e6444f92aa22287204f
dannydelott <dannydelott@gmail.com>
2015-05-23 00:27:05 -0700

    Remove view instantiotions.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-// Collection
-var weather = new Weather();
-
-// Views
-var app = new AppView();
-
-var title = new TitleView();
-
-var input = new InputView({
-  collection: weather
-});
-
-var list = new ListView({
-  collection: weather
+var app = new AppView({
+  collection: new Weather()
 });
