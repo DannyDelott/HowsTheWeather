42be71e3a4ce3c7f7fa167b466546dd6afd12eed
dannydelott <dannydelott@gmail.com>
2015-05-23 00:32:40 -0700

    Update TitleView.js
    - Add return statement to render method.
    - Remove template property.

--- js/views/TitleView.js
+++ js/views/TitleView.js
@@ -1,17 +1,14 @@
 var TitleView = Backbone.View.extend({
 
-  el: '#content',
-
-  template: _.template('<h1><%= pageTitle %></h1>'),
+  el: '<h1>',
 
   initialize: function() {
     this.render();
   },
 
   render: function() {
-    this.$el.html(this.template({
-      pageTitle: 'How\'s the weather?'
-    }));
+    this.$el.text('How\'s the weather?');
+    return this;
   }
 
 });
