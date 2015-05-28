8d79448606588573bc7f133af728b4d4700c4922
dannydelott <dannydelott@gmail.com>
2015-05-27 23:22:03 -0700

    Refactor addWeatherEntry

--- js/collections/Weather.js
+++ js/collections/Weather.js
@@ -4,26 +4,17 @@ var Weather = Backbone.Collection.extend({
 
   addWeatherEntry: function(zipcode) {
 
-    this.getData(
-      zipcode,
-      function(data) {
-        this.add(new WeatherEntry({
-          zipcode: zipcode,
-          city: data.name,
-          weather: data.main.temp + '°F'
-        }));
-      }.bind(this)
-    );
-
-  },
-
-  getData: function(zipcode, callback) {
-
     $.get(
       'http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial',
       function(data) {
-        callback(data);
-      }
+        this.add(
+          new WeatherEntry({
+            zipcode: zipcode,
+            city: data.name,
+            weather: data.main.temp + '°F'
+          })
+        );
+      }.bind(this)
     );
   }
 
